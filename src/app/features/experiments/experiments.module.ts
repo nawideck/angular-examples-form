2f09bf67b463d1e71826b540880007b0cb6fc20d
Anthony Batifol <anthony.batifol@altran.com>
2019-12-06 09:17:53 +0100

    feat(experiments): create experiments module
    create experiments module

---
+++ src/app/features/experiments/experiments.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExperimentsRoutingModule } from './experiments-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ExperimentsRoutingModule]
+})
+export class ExperimentsModule {}
